d6ae2535596117c319a122531e5ff8f4461f2b70
Ayoub Echcharrat <ayoub.ech-charrat@hotmail.fr>
2023-01-18 17:04:04 +0100

    (remis la version de sidebar de base en commentaire à la fin)

--- scripts/search.js
+++ scripts/search.js
@@ -359,3 +359,51 @@ function filterByTag() {
         eventTagUstensile(newRecipesList)
     }  */
 
+/*     function searchbar() {
+        const searchbar = document.getElementById('searchbar')
+        // eslint-disable-next-line no-undef
+        const recipes = getRecipes()
+ 
+        function eventSearchbar() {
+            let newRecipesList = []
+            if (searchbar.value.length < 3) {
+                newRecipesList = recipes
+                eventTagIngredient(recipes)
+                eventTagAppareil(recipes)
+                eventTagUstensile(recipes)
+                // eslint-disable-next-line no-undef
+                recipesCardHUB(recipes)
+            } else {
+                recipes.forEach(recipe => {
+                    let name = recipe.name.toLowerCase() //recherche par nom
+                    let description = recipe.description.toLowerCase() //recherche par descr.
+ 
+                    let ingredients = recipe.ingredients
+                    ingredients.forEach(elem => {
+                        let ingredient = elem.ingredient.toLowerCase() //recherche par ingre.
+                        if (name.includes(searchbar.value) || description.includes(searchbar.value) || ingredient.includes(searchbar.value)) {
+                            if (newRecipesList.length === 0) {
+                                newRecipesList.push(recipe)
+                            } else {
+                                if (newRecipesList[newRecipesList.length - 1].name === recipe.name) {
+                                    return
+                                } else {
+                                    newRecipesList.push(recipe)
+                                    // eslint-disable-next-line no-undef
+                                    recipesCardHUB(newRecipesList)
+                                }
+                            }
+                        } else {
+                            // eslint-disable-next-line no-undef
+                            recipesCardHUB(newRecipesList)
+                        }
+                    });
+                });
+                eventTagIngredient(newRecipesList)
+                eventTagAppareil(newRecipesList)
+                eventTagUstensile(newRecipesList)
+            }
+        }
+        searchbar.addEventListener('keyup', eventSearchbar)
+    }
+    searchbar()  */
